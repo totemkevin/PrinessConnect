84ca47bf8306cda6ab2961f449e6418748ed0cb0
kevin <totemkevin830108@gmail.com>
2018-11-17 01:41:53 +0800

    Initial commit

---
+++ src/main/java/com/prinessConnect/PrinessConnectApplication.java
@@ -0,0 +1,12 @@
+package com.prinessConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrinessConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrinessConnectApplication.class, args);
+	}
+}
